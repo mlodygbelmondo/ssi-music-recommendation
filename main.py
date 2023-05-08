591046027d67c24fd99c0873ca49bb6677ac235a
dyzioq <dycha2217@gmail.com>
2023-05-08 11:10:48 +0200

    Prawie gotowa klasa ProcessingData

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
+import random as rn
 import pandas as pd
+import seaborn as sns
+
+dataFrame = pd.read_csv(r'Spotify_Youtube.csv')
+class ProcessingData:
+    @staticmethod
+    def transformate(dataFrame):#usuwamy niepotrzebne kolumny
+        neededColumns=["Track", "Artist", "Danceability", "Energy", "Key", "Loudness", "Speechiness", "Acousticness", "Instrumentalness", "Liveness", "Valence", "Tempo"]
+        for column in dataFrame.columns:
+            if column not in neededColumns:
+                del dataFrame[column]
+        return dataFrame
+    @staticmethod
+    def shuffle(x):
+        for i in range(len(x) - 1, 0, -1):
+            j = rn.randint(0, i - 1)
+            x.iloc[i], x.iloc[j] = x.iloc[j], x.iloc[i]
+
+    @staticmethod
+    def normalization(x):#normalizacja 'transformowanej' bazy
+        for column in x.columns:
+            if column in ["Artist", "Track"]:
+                continue
+            lista = list(x.loc[:, column])
+            mini = min(lista)
+            maxi = max(lista)
+            for i in range(len(x)):
+                x.at[i, column] = (x.at[i, column] - mini) / (maxi - mini)
+
 
 def get_user_prompt():
     user_string_data = []
@@ -16,5 +45,10 @@ def main():
     music_data = pd.read_csv('Spotify_Youtube.csv')
     print(music_data.head())
 
-main()
+#main()
+print(dataFrame.head())
+dataFrameFormated = ProcessingData.transformate(dataFrame)
+print(dataFrame.head())
+ProcessingData.normalization(dataFrameFormated)
+print(dataFrame.head())
 
